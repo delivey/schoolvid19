e0d18e14aecc2e397d2ff7918598d5512685a797
delivey <deliveydev@gmail.com>
2020-05-20 08:12:27 +0300

    added dynamic urls

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/country/<cnt>")
+def country(cnt):
+    return render_template("country.html", cnt=cnt)
