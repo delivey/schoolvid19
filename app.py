31ec08fa38d2915f84dbdb87afd8196c8891173b
delivey <deliveydev@gmail.com>
2020-05-20 18:59:42 +0300

    search is now working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -14,3 +15,11 @@ def country(cnt):
 def countries():
     if request.method == "GET":
         return render_template("countries.html")
+    else:
+        connection = sqlite3.connect('countries.db') # connects to db
+        db = connection.cursor() # creates the cursor for db connection
+
+        search = request.form.get("cName")
+        result = db.execute("SELECT country FROM names WHERE country=(?)", (search,)).fetchone()[0]
+        connection.commit()
+        return render_template("countries.html", result=result)
