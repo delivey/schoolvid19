e3b0e1cc53f031ca22e64ca0039f122d355a7075
delivey <deliveydev@gmail.com>
2020-05-20 18:20:45 +0300

    added /countries, template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,8 @@ def index():
 @app.route("/country/<cnt>")
 def country(cnt):
     return render_template("country.html", cnt=cnt)
+
+@app.route("/countries", methods=["GET", "POST"])
+def countries():
+    if request.method == "GET":
+        return render_template("countries.html")
